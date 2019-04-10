d1f2bb5da511d636c7b5a4018e1e29394bc8531d
gyuzliev <petar.gyuzliev@gmail.com>
2019-04-10 17:04:26 +0300

    Exercise added

---
+++ JavaScript/Tehnlogy-Fundamentals/1-BASIC-SYNTAX-CONDITIONAL-STATEMENTS-AND-LOOPS/Lab/11-Sum-of-Odd-Numbers.js
@@ -0,0 +1,14 @@
+function solve(n) {
+    let sum = 0;
+    let currentOddNumber = 1;
+    for (let i = 0; i < n; i++) {
+        console.log(currentOddNumber);
+            sum += currentOddNumber;
+            currentOddNumber += 2;
+    }
+
+    console.log(`Sum: ${sum}`);
+
+}
+
+solve(5);
